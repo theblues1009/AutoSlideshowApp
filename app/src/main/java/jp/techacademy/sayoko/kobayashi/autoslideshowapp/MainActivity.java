198fa408d12cb8ebd90f6aa43169f895be82b57a
kobayashisayoko <theblues1009@gmail.com>
2017-06-22 20:40:30 +0900

    アクセス拒否時のボタン押下にトーストを表示しアプリ終了。

--- app/src/main/java/jp/techacademy/sayoko/kobayashi/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/sayoko/kobayashi/autoslideshowapp/MainActivity.java
@@ -13,17 +13,29 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    Cursor cursor;
+    Cursor cursor = null;
+    boolean csp = false;
+    Timer mTimer;
+
+    Button mNextToButton;
+    Button mPreviousButton;
+    Button mStartPauseButton;
+
+    double mTimerSec = 0.0;
+
 
     private static final int PERMISSIONS_REQUEST_CODE = 100;
 
@@ -31,8 +43,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
 
         //Android 6.0以降の場合
@@ -44,11 +54,130 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 //許可されていないので許可ダイアログを表示する
                 requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
+
             }
             //Android 5系以下の場合
         } else {
             getContentsInfo();
         }
+        final Handler mHandler = new Handler();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            mNextToButton = (Button) findViewById(R.id.nextTo_button);
+            mNextToButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (csp == false) {
+                        toastMake("画像へのアクセスが拒否されているためアプリを終了します");
+                        finish();
+                    } else {
+                        if (cursor.moveToNext()) {
+                            setImageView();
+                        } else { cursor.moveToFirst();
+                            setImageView();
+                        }
+
+
+                    }
+                }
+            });}
+        else{mNextToButton = (Button) findViewById(R.id.nextTo_button);
+            mNextToButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (csp == false) {
+                        toastMake("画像へのアクセスが拒否されているためアプリを終了します");
+                        finish();
+                    } else {
+                        if (cursor.moveToNext()) {
+                            setImageView();
+                        } else { cursor.moveToFirst();
+                            setImageView();
+                        }
+
+
+                    }
+                }
+            });
+
+        }
+
+        mPreviousButton = (Button) findViewById(R.id.previous_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (csp == false) {
+                    toastMake("画像へのアクセスが拒否されているためアプリを終了します");
+                    finish();
+                } else {
+                    if (cursor.moveToPrevious()) {
+                        setImageView();
+
+                    } else {
+
+                        cursor.moveToLast();
+                        setImageView();
+                    }
+
+                }
+            }
+
+        });
+
+
+        mStartPauseButton = (Button) findViewById(R.id.start_pause_button);
+        mStartPauseButton.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick(View v) {
+                if (cursor == null) {
+                    toastMake("画像が読み込めません。");
+                    finish();
+                } else {
+                    mStartPauseButton.setText("停止");
+                    mNextToButton.setEnabled(false);
+                    mPreviousButton.setEnabled(false);
+                    if (mTimer == null) {
+                        mTimer = new Timer();
+                        mTimer.schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                mTimerSec += 0.1;
+
+
+                                mHandler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        if (cursor.moveToNext()) {
+
+                                        } else {
+
+                                            cursor.moveToFirst();
+                                        }
+                                        setImageView();
+
+
+                                    }
+                                });
+
+                            }
+                        }, 100, 2000);
+
+
+                    } else {
+                        mTimer.cancel();
+                        mTimer = null;
+                        mStartPauseButton.setText("再生");
+                        mNextToButton.setEnabled(true);
+                        mPreviousButton.setEnabled(true);
+                    }
+                }
+            }
+
+
+        });
     }
 
     @Override
@@ -64,8 +193,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-
     private void getContentsInfo() {
         //画像の情報を取得する
         ContentResolver resolver = getContentResolver();
@@ -76,28 +203,23 @@ public class MainActivity extends AppCompatActivity {
                 null,//フィルタ用パラメータ
                 null//ソート
         );
+    csp = true;
 
-        Button mLastButton;
 
-        mLastButton = (Button) findViewById(R.id.last_button);
+    }
 
-        mLastButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (cursor.moveToFirst()) {
-                    do {
-                        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                        Long id = cursor.getLong(fieldIndex);
-                        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
-                        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                        imageView.setImageURI(imageUri);
-                    } while (cursor.moveToNext());
-                }
-                cursor.close();
-            }
-        });
+    private void setImageView() {
+        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+        Long id = cursor.getLong(fieldIndex);
+        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageURI(imageUri);
+    }
 
-        }
 
+    private void toastMake(String message) {
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+        toast.show();
+    }
 }
